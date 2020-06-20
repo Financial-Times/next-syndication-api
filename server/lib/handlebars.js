047656624142322350c8cdbbe5424745becdd472
estefania morton <estefaniamorton@estefanias-MacBook-Pro.local>
2020-06-19 20:10:05 -0500

    unit tests for handlebars 🐿 v2.13.0

--- server/lib/handlebars.js
+++ server/lib/handlebars.js
@@ -5,11 +5,11 @@ const Handlebars = require('handlebars');
 module.exports = function () {
 	const helpers = {
 		ifEquals: function (a, b, options) {
-            if (a === b) {
-                return options.fn(this);
-            }
-            return options.inverse(this);
-        }
+			if (a === b) {
+				return options.fn(this);
+			}
+			return options.inverse(this);
+		}
 	};
 
 	const handlebars = Handlebars;
