bfb86ec6c69fd4d6a5726220a34049c59873b7f2
estefania morton <estefaniamorton@estefanias-MacBook-Pro.local>
2020-06-19 20:12:07 -0500

    using tabs instead of spaces 🐿 v2.13.0

--- test/server/lib/handlebars.spec.js
+++ test/server/lib/handlebars.spec.js
@@ -4,13 +4,13 @@ const { expect } = require('chai');
 const Handlebars = require('../../../server/lib/handlebars')();
 
 describe('Handlebars with extended helper', function () {
-    it('Should register an ifEquals helper', function () {
-        expect(Handlebars.helpers).to.haveOwnProperty('ifEquals');
-    });
+	it('Should register an ifEquals helper', function () {
+		expect(Handlebars.helpers).to.haveOwnProperty('ifEquals');
+	});
 
-    it('should render a template using ifEquals helper', function () {
-        const template = Handlebars.compile('{{#ifEquals thing1 "thing1"}}first{{else}}not first{{/ifEquals}} {{#ifEquals thing1 "thing2"}}second{{else}}not second{{/ifEquals}}');
-        const rendered = template({thing1: 'thing1', thing2: 'thing2'});
-        expect(rendered).to.eql('first not second')
+	it('should render a template using ifEquals helper', function () {
+		const template = Handlebars.compile('{{#ifEquals thing1 "thing1"}}first{{else}}not first{{/ifEquals}} {{#ifEquals thing1 "thing2"}}second{{else}}not second{{/ifEquals}}');
+		const rendered = template({thing1: 'thing1', thing2: 'thing2'});
+		expect(rendered).to.eql('first not second')
 	});
 });
