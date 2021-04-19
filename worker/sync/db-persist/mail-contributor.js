cedbf9c2a315a142c4bb2336ba7cc259b45741a9
AniaMakes <anna.bebb@ft.com>
2021-04-19 11:41:42 +0100

    wired up email platform functionality

--- worker/sync/db-persist/mail-contributor.js
+++ worker/sync/db-persist/mail-contributor.js
@@ -8,17 +8,18 @@ const { Logger } = require('../../../server/lib/logger');
 
 //const moment = require('moment');
 const moment = require('moment-timezone');
-const nodemailer = require('nodemailer');
+// const nodemailer = require('nodemailer');
 
 const {
 	CONTRIBUTOR_EMAIL
 } = require('config');
+const { EMAIL_PLATFORM_API_KEY, EMAIL_PLATFORM_URL, ACCOUNTS_EMAIL } = process.env
 
 const pg = require('../../../db/pg');
 
 const Handlebars = handlebars();
 
-const transporter = nodemailer.createTransport(JSON.parse(JSON.stringify(CONTRIBUTOR_EMAIL.transport)));
+// const transporter = nodemailer.createTransport(JSON.parse(JSON.stringify(CONTRIBUTOR_EMAIL.transport)));
 
 const HTML = Handlebars.compile(fs.readFileSync(path.resolve('./server/views/partial/email_contributor_body.html.hbs'), 'utf8'), { noEscape: true });
 const TXT = Handlebars.compile(fs.readFileSync(path.resolve('./server/views/partial/email_contributor_body.txt.hbs'), 'utf8'), { noEscape: true });
@@ -49,24 +50,47 @@ module.exports = exports = async (event) => {
 		//		}
 
 		const [contract] = await db.syndication.get_contract_data([event.contract_id]);
-
 		event.contract = contract;
 
 		log.debug('RECEIVED => ', event);
 
 		event.displayDate = moment.tz(event.time, CONTRIBUTOR_EMAIL.timezone).format(CONTRIBUTOR_EMAIL.date_display_format);
 
-		const transport = {
-			from: CONTRIBUTOR_EMAIL.from,
-			to: CONTRIBUTOR_EMAIL.to,
+		/// --- Email Platform Implementation --- 
+		const emailPlatformHeaders = {
+			'Content-Type': 'application/json',
+			'Authorization': EMAIL_PLATFORM_API_KEY
+		}
+
+		const emailPlatformBody = {
+			to: { address: [CONTRIBUTOR_EMAIL.to, ACCOUNTS_EMAIL] },
+			from: { address: CONTRIBUTOR_EMAIL.from, name: 'With Contributor Payment Syndication alert bot' },
 			subject: CONTRIBUTOR_EMAIL.subject,
-			html: HTML(event),
-			text: TXT(event)
-		};
+			htmlContent: HTML(event),
+			plainTextContent: TXT(event)
+		}
 
-		const res = await transporter.sendMail(transport);
+		const res = await fetch(EMAIL_PLATFORM_URL, {
+			method: 'POST',
+			headers: emailPlatformHeaders,
+			body: JSON.stringify(emailPlatformBody)
+		})
 
-		log.info(`${MODULE_ID} MAIL SENT =>`, res);
+		if (res.total_rejected_recipients !== 0) {
+			log.info('Some emails were not delivered. Not delivered count', res.total_rejected_recipients)
+		}
+
+		// const transport = {
+		// from: CONTRIBUTOR_EMAIL.from,
+		// to: CONTRIBUTOR_EMAIL.to,
+		// subject: CONTRIBUTOR_EMAIL.subject,
+		// html: HTML(event),
+		// text: TXT(event)
+		// };
+
+		// const res = await transporter.sendMail(transport);
+
+		log.info(`${MODULE_ID} MAIL SENT =>`, res.json());
 
 		const data = contributor_payment && contributor_payment.contract_id !== null
 			? contributor_payment
