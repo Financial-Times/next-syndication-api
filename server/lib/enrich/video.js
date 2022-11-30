c1f70fa82dccdf7328f5b4fa971d94418ad66878
Andy Gout <andygout@hotmail.co.uk>
2022-11-30 11:38:04 +0000

    Add handling for undefined content.download in video enrichment

--- server/lib/enrich/video.js
+++ server/lib/enrich/video.js
@@ -15,7 +15,10 @@ module.exports = exports = function video(content, contract, graphicSyndicationF
 	content.extension = DOWNLOAD_ARCHIVE_EXTENSION;
 
 	content.download = Array.from(content.attachments).reverse().find(item => item.mediaType === DOWNLOAD_MEDIA_TYPES.video);
-	content.download.extension = (content.download) ? mime.extension(content.download.mediaType) : '';
+
+	if (content.download) {
+		content.download.extension = (content.download) ? mime.extension(content.download.mediaType) : '';
+	}
 
 	content.captions = content.attachments.filter(item => item.mediaType === DOWNLOAD_MEDIA_TYPES.caption);
 
