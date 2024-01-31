2c9d2399c77608014d73d58f89278381ed5ad460
Estefania Morton <estefania.morton@ft.com>
2024-01-31 15:25:57 +0000

    get activeOrder from contract not contract_data

--- server/controllers/get-contract-by-id.js
+++ server/controllers/get-contract-by-id.js
@@ -38,8 +38,8 @@ module.exports = exports = async (req, res, next) => {
 			if (req.query.save !== '0') {
 				let contract_data = reformatSalesforceContract(JSON.parse(JSON.stringify(contract)));
 				contract_data.last_updated = new Date();
-				if (contract_data.orders) {
-					const activeOrder = contract_data.orders.find(order => order.status === 'Activated');
+				if (contract.orders) {
+					const activeOrder = contract.orders.find(order => order.status === 'Activated');
 					contract_data.current_start_date = new Date(activeOrder.startDate);
 					contract_data.current_end_date = new Date(activeOrder.endDate);
 				}
