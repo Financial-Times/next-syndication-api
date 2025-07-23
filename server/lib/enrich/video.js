eaf862cd11f868caec1e65cec13aaf9aad017220
Nazeh Abel <talentednazeh@gmail.com>
2025-07-23 14:58:57 +0100

    chore: Remove graphicSyndication flag in the server/controllers code

--- server/lib/enrich/video.js
+++ server/lib/enrich/video.js
@@ -6,21 +6,27 @@ const article = require('./article');
 
 const { DOWNLOAD_ARCHIVE_EXTENSION, DOWNLOAD_MEDIA_TYPES } = require('config');
 
-module.exports = exports = function video(content, contract, graphicSyndicationFlag) {
-	content = article(content, contract, graphicSyndicationFlag);
+module.exports = exports = function video(content, contract) {
+	content = article(content, contract);
 
-	content.hasTranscript = !!(content.bodyHTML__CLEAN && content.bodyHTML__CLEAN.length);
+	content.hasTranscript = !!(
+		content.bodyHTML__CLEAN && content.bodyHTML__CLEAN.length
+	);
 	content.transcriptExtension = content.extension;
 
 	content.extension = DOWNLOAD_ARCHIVE_EXTENSION;
 
-	content.download = Array.from(content.attachments).reverse().find(item => item.mediaType === DOWNLOAD_MEDIA_TYPES.video);
+	content.download = Array.from(content.attachments)
+		.reverse()
+		.find((item) => item.mediaType === DOWNLOAD_MEDIA_TYPES.video);
 
 	if (content.download) {
 		content.download.extension = mime.extension(content.download.mediaType);
 	}
 
-	content.captions = content.attachments.filter(item => item.mediaType === DOWNLOAD_MEDIA_TYPES.caption);
+	content.captions = content.attachments.filter(
+		(item) => item.mediaType === DOWNLOAD_MEDIA_TYPES.caption
+	);
 
 	return content;
 };
