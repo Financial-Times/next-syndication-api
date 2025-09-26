803de55d718e7d766262a6c4bc077d31c920889b
manasiSantFT <manasi.sant@ft.com>
2025-09-26 13:34:39 +0100

    Exporting hasFlourishGraphics

--- server/lib/resolve/index.js
+++ server/lib/resolve/index.js
@@ -6,6 +6,7 @@ exports.canDownload = require('./canDownload');
 exports.canBeSyndicated = require('./canBeSyndicated');
 exports.downloaded = require('./downloaded');
 exports.embargoPeriod = require('./embargoPeriod');
+exports.hasFlourishGraphics = require('./hasFlourishGraphics');
 exports.hasGraphics = require('./hasGraphics');
 exports.lang = require('./lang');
 exports.publishedDate = require('./publishedDate');
