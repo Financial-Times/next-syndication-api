fc92bc63f54a63f00539a262edff523b621dff3a
Alex Muller <alex.muller@ft.com>
2025-12-04 10:26:32 +0000

    Disable env_check script

--- scripts/env_check.js
+++ scripts/env_check.js
@@ -1,4 +1,8 @@
 
+console.log('We think this check is unnecessary because we do not experience issues when the key contains a slash'); // eslint-disable-line no-console
+
+process.exit(0);
+
 if (process.env.AWS_SECRET_ACCESS_KEY.includes('/') || process.env.AWS_SECRET_ACCESS_KEY.includes('\\')) {
 	console.log('AWS_SECRET_ACCESS_KEY contains slashes. Regenerate the AWS AKIA/key pair and try again. Slashes in AWS secrets result in a working app but syndication functionality not working.') // eslint-disable-line no-console
 	process.exit(1); // Exit the script with a non-zero exit code to indicate failure
