c03d04925996c97e696b4b82585197212eba0e0a
ytcleon <ytcleon@gmail.com>
2026-04-13 16:37:36 +0100

    chore: prettify get-subject-access-request

--- server/controllers/get-subject-access-request.js
+++ server/controllers/get-subject-access-request.js
@@ -3,39 +3,50 @@
 const { Logger } = require('../lib/logger');
 
 module.exports = exports = async (req, res) => {
-	const log = new Logger({req, res, source: 'controllers/get-subject-access-request'});
+	const log = new Logger({
+		req,
+		res,
+		source: 'controllers/get-subject-access-request',
+	});
 
-    try {
-        const {
-            locals: { $DB: db },
-        } = res;
-        const { uuid, email } = req.body;
-        const userIdentifier = uuid || email;
+	try {
+		const {
+			locals: { $DB: db },
+		} = res;
+		const { uuid, email } = req.body;
+		const userIdentifier = uuid || email;
 
-        if (!userIdentifier) {
-            return res.status(400).json({
-                code: 'SUBJECT_ACCESS_REQUEST_MISSING_USER_IDENTIFIER',
-                error: 'Missing user identifier. Either uuid or email must be provided in the request\'s body.',
-            });
-	    }
+		if (!userIdentifier) {
+			return res.status(400).json({
+				code: 'SUBJECT_ACCESS_REQUEST_MISSING_USER_IDENTIFIER',
+				error: 'Missing user identifier. Either uuid or email must be provided in the request\'s body.',
+			});
+		}
 
-        const [result] = await db.syndication.get_user_subject_access_data([userIdentifier]);
+		const [result] = await db.syndication.get_user_subject_access_data([
+			userIdentifier,
+		]);
 
-        if (result?.get_user_subject_access_data?.data?.syndicationUserData?.user) {
-            log.info('SUBJECT_ACCESS_REQUEST_PROCESSED', { userIdentifier });
-            return res.status(200).json(result.get_user_subject_access_data);
-        } else {
-            log.info('SUBJECT_ACCESS_REQUEST_USER_NOT_FOUND', { userIdentifier });
-            return res.sendStatus(404);
-        }
-    } catch (error) {
-        log.error('SUBJECT_ACCESS_REQUEST_FAILED_TO_GET_USER_DATA', {
-            event: 'SUBJECT_ACCESS_REQUEST_FAILED_TO_GET_USER_DATA',
-            error,
-        });
-        return res.status(500).json({
+		if (
+			result?.get_user_subject_access_data?.data?.syndicationUserData
+				?.user
+		) {
+			log.info('SUBJECT_ACCESS_REQUEST_PROCESSED', { userIdentifier });
+			return res.status(200).json(result.get_user_subject_access_data);
+		} else {
+			log.info('SUBJECT_ACCESS_REQUEST_USER_NOT_FOUND', {
+				userIdentifier,
+			});
+			return res.sendStatus(404);
+		}
+	} catch (error) {
+		log.error('SUBJECT_ACCESS_REQUEST_FAILED_TO_GET_USER_DATA', {
+			event: 'SUBJECT_ACCESS_REQUEST_FAILED_TO_GET_USER_DATA',
+			error,
+		});
+		return res.status(500).json({
 			code: 'SUBJECT_ACCESS_REQUEST_FAILED_TO_GET_USER_DATA',
 			error: 'Failed to retrieve user data from the database.',
 		});
-    }
+	}
 };
